99168988e0869adde42b187d6c78bbcf3fa6d985
aamirkhan37557 <aamirkhan3757@gmail.com>
2021-04-25 15:35:38 +0500

    Create laravel _register.blade.php

---
+++ laravel _register.blade.php
@@ -0,0 +1,33 @@
+1. connect laravel with db
+2.   after installing laravel fortify  create route for register user in web.php and make design for it 
+
+
+                                      use Laravel\Fortify\Fortify;
+
+                                   Fortify::registerView(function () {
+                                     return view('admincredentials/register');
+                                     });
+                                      
+3. in register view first display error 
+                           
+                                                    @if(count($errors) > 0)
+                      <div class="p-1">
+                          @foreach($errors->all() as $error)
+                          <div class="alert alert-warning alert-danger fade show" role="alert">{{$error}} <button type="button" class="close"
+                                  data-dismiss="alert" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                              </button></div>
+                          @endforeach
+                      </div>
+                      @endif
+                      
+4. now register the  user 
+
+ <form  action="{{route('register')}}" method="post">@csrf
+ 
+ 5. check all the in fields names like name,email,password,confirm password etc
+ 
+ 6.after succssfully registeration it will redirect to home route create home route for it
+
+
+                                     
